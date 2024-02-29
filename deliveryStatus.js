48fce6192f22e3b6a10f3f702339d19e020455cf
jwsearle18 <144712016+jwsearle18@users.noreply.github.com>
2024-02-28 17:10:43 -0700

    deliveryStatus websocket simulation

---
+++ deliveryStatus.js
@@ -0,0 +1,23 @@
+document.addEventListener("DOMContentLoaded", function() {
+    const statusMessages = [
+      "Order Sent!",
+      "Order Being Picked up!",
+      "Order Being Stocked!",
+      "Your Vacation Rental is Fully Airestocked!"
+    ];
+    
+    let currentIndex = 0; 
+    
+    function updateStatus() {
+      document.getElementById("status").innerText = statusMessages[currentIndex];
+      
+      currentIndex++;
+      
+      if (currentIndex < statusMessages.length) {
+        setTimeout(updateStatus, 3000); 
+      }
+    }
+    
+    setTimeout(updateStatus, 1000);
+  });
+  
